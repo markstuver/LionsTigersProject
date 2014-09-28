8013141d968e260e86114b69487c998d7df5b4c6
Mark Stuver <mark@stuverfamily.com>
2014-09-28 18:49:42 -0400

    Created multiple instances of Tiger.

--- Lions__Tigers/ViewController.swift
+++ Lions__Tigers/ViewController.swift
@@ -22,14 +22,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    // Create instance of Tiger struct
-    var firstTiger = Tiger()
+    
+        // Create instance of Tiger struct
+        var firstTiger = Tiger()
         
         // Set properties of firstTiger using Tiger properties
         firstTiger.name = "Tigger"
         firstTiger.breed = "Bengal Tiger"
         firstTiger.age = 3
         firstTiger.image = UIImage(named: "BengalTiger.jpg")
+
+        
+        // Create instance of Tiger struct
+        var secondTiger = Tiger()
+        
+        // Set properties of firstTiger using Tiger properties
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+
+        
+        // Create instance of Tiger struct
+        var thirdTiger = Tiger()
+        
+        // Set properties of firstTiger using Tiger properties
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+
+        
+        // Create instance of Tiger struct
+        var fourthTiger = Tiger()
+        
+        // Set properties of firstTiger using Tiger properties
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 3
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+
+        
+        
+        
         
         
         // Set View Labels using firstTiger's Tiger properties
