2f2b43b481bdcf2ad397a6e3baaa3c492b71916b
Mark Stuver <mark@stuverfamily.com>
2014-10-04 22:52:11 -0400

    Adding Chuff Function as well as Adding Random Fact Function

--- Lions__Tigers/Tigers.swift
+++ Lions__Tigers/Tigers.swift
@@ -10,6 +10,7 @@ import Foundation
 // import UIKit for UIImage capabilities
 import UIKit
 
+
 /* Struct is like a class but struts are always copied and do not participate in reference counting.
 //Structs cannot be inherited or type-cast */
 /// Tiger struct will be called when an instance of Tiger is to be created.
@@ -20,5 +21,74 @@ struct Tiger {
     var breed = ""
     var age = 0
     var image = UIImage(named: "")
+    
+    // Function in Strict that is called when tiger makes a noise.
+    func chuff() {
+        println("Tiger: Chuff Chuff")
+    }
+    
+    // Function in Strict that takes a parameter and will run the chuff() function as many times as the parameter shows
+    func chuffANumberOfTimes (numberOfTimes: Int) {
+        
+        for var chuff = 0; chuff < numberOfTimes; ++chuff {
+            
+            self.chuff()
+        }
+    }
+    
+    // fuction with 2 parameters and uses Control Flow
+    func chuffANumberOfTimes (numberOfTimes: Int, isLoud: Bool) {
+        
+        // determine how many times to loop code
+        for var chuffTimes = 1; chuffTimes <= numberOfTimes; chuffTimes++ {
+            
+            // Control Flow
+            // if isLoud (true) run chuff() function (prints "Chuff Chuff" to console
+            if isLoud {
+                chuff()
+            }
+            // else (isLoud = false) print the following to console
+            else {
+                
+                println("Tiger: purr purr")
+            }
+        }
+    }
+    
+    // function take 1 parameter and returns an Int
+    func ageInTigerYearsFromAge(regularAge: Int) -> Int {
+        
+        let newAge = regularAge * 3
+        return newAge
+        
+    }
+    
+    // function returns a string when called
+    func randomFact() ->String {
+        
+        // create a random number
+        let randomNumber = Int(arc4random_uniform(UInt32(3)))
+        
+        // create variable instance
+        var randomFact:String
+        
+        // determine the string value based on the random number
+        if randomNumber == 0 {
+            
+            randomFact = "The Tiger is the biggest species in the cat family"
+            
+        }
+        else if randomNumber == 1 {
+            
+            randomFact = "Tigers can reach a length of 3.3 meters"
+        }
+        else {
+            randomFact = "A group of tigers is know as an 'ambush' or 'streak'"
+        }
+        
+        // return the random string
+        return randomFact
+    }
+    
 }
 
--- Lions__Tigers/ViewController.swift
+++ Lions__Tigers/ViewController.swift
@@ -15,9 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var breedLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var randomFactLabel: UILabel!
     
     // Create an array with type Tiger and setting as an empty array
     var myTigers:[Tiger] = []
+    
+    var currentIndex = 0
     
     
     override func viewDidLoad() {
@@ -31,6 +34,12 @@ class ViewController: UIViewController {
         firstTiger.breed = "Bengal Tiger"
         firstTiger.age = 3
         firstTiger.image = UIImage(named: "BengalTiger.jpg")
+    
+        firstTiger.age = firstTiger.ageInTigerYearsFromAge(firstTiger.age)
+            
+            
+        firstTiger.chuff()
+        firstTiger.chuffANumberOfTimes(5, isLoud: false)
 
         // Add firstTiger into the myTigers array using .append
         myTigers.append(firstTiger)
@@ -41,28 +50,43 @@ class ViewController: UIViewController {
         secondTiger.breed = "Indochinese Tiger"
         secondTiger.age = 2
         secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
-
+        
+        secondTiger.age = firstTiger.ageInTigerYearsFromAge(secondTiger.age)
+        
+        secondTiger.chuff()
+        
         var thirdTiger = Tiger()
         thirdTiger.name = "Jacob"
         thirdTiger.breed = "Malayan Tiger"
         thirdTiger.age = 4
         thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        thirdTiger.age = firstTiger.ageInTigerYearsFromAge(thirdTiger.age)
 
         var fourthTiger = Tiger()
         fourthTiger.name = "Spar"
         fourthTiger.breed = "Siberian Tiger"
         fourthTiger.age = 3
         fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
+        fourthTiger.age = firstTiger.ageInTigerYearsFromAge(fourthTiger.age)
 
         // Add secondTiger, thirdTiger & fourthTiger to myTigers array using Logic Operator short-hand
         myTigers += [secondTiger, thirdTiger, fourthTiger]
         
+        
+        firstTiger.chuffANumberOfTimes(3)
+        secondTiger.chuffANumberOfTimes(2)
+        
         // Set View Labels using firstTiger's Tiger properties
-        nameLabel.text = firstTiger.name
-        breedLabel.text = firstTiger.breed
-        ageLabel.text = "\(firstTiger.age)"
-        imageView.image = firstTiger.image
+        self.nameLabel.text = firstTiger.name
+        self.breedLabel.text = firstTiger.breed
+        self.ageLabel.text = "\(firstTiger.age)"
+        self.imageView.image = firstTiger.image
+        self.randomFactLabel.text = firstTiger.randomFact()
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -74,10 +98,27 @@ class ViewController: UIViewController {
     // Updates the UI objects with random instances of the Tiger
     @IBAction func nextButtonPressed(sender: UIBarButtonItem) {
         
-        // Create a random number from 0 to the count of the myTigers array
-        let randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
         
-        // Create instance of Tiger and set it to the tiger that is at the randomIndex in the array
+        /// Create a variable that will be used to create a random number
+        var randomIndex:Int
+        
+        // Execute the random number code once, if the while condition stays true, re-execute the random number code, if the while is false, break from the DO/WHILE loop.
+        do {
+        
+        // Create a random number from 0 to the count of the myTigers array
+        randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        
+          // if the current tiger is the same as the random number, run the random number code againg until it is a different tiger
+        } while currentIndex == randomIndex
+    
+        currentIndex = randomIndex
+        
+        
+        // THE FOLLOWING COMMENTED CODE WAS MOVED INTO THE DO/WHILE CODE ABOVE
+//        // Create a random number from 0 to the count of the myTigers array
+//        let randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        
+        // Create instance of Tiger Struct and set it to the tiger that is at the randomIndex in the array
         let currentTiger = myTigers[randomIndex]
         
         // THE FOLLOWING COMMENTED CODE WAS MOVED INTO THE UIView.transitionWithView CLOSURE
@@ -88,13 +129,14 @@ class ViewController: UIViewController {
        // imageView.image = currentTiger.image
         
         // Set the view to make an animated transition while the UI objects are being set to the next currentTiger
-        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+        UIView.transitionWithView(self.view, duration: 1, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
             
             //Set UI objects to the current tiger's properties
             self.nameLabel.text = currentTiger.name
             self.ageLabel.text = "\(currentTiger.age)"
             self.breedLabel.text = currentTiger.breed
             self.imageView.image = currentTiger.image
+            self.randomFactLabel.text = currentTiger.randomFact()
             
             }, completion: { (finished: Bool) -> () in
         })
